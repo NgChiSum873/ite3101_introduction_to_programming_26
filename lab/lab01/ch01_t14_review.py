cea574546c1665b4e0be243f6020d00b9833a00e
NgChiSum873 <260061806@stu.vtc.edu.hk>
2026-09-04 14:08:35 +0000

    lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,9 +1,9 @@
 skill_completed = "Python Syntax"
 exercises_completed = 13
 
-points_per_ecercise = 5
+points_per_exercise = 5
 
 point_total = 100
-point_total += exercises_completed * points_per_ecercise
+point_total += exercises_completed * points_per_exercise
 
 print("I got " + str(point_total) + " points!")
