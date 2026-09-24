996ccb50a7fe6556bc688c97c3a519f29fba11a7
NgChiSum873 <260061806@stu.vtc.edu.hk>
2026-09-24 08:30:57 +0000

    [skip actions] lab/lab06/ch06_t02_function_junction.py

--- lab/lab06/ch06_t02_function_junction.py
+++ lab/lab06/ch06_t02_function_junction.py
@@ -1,7 +1,8 @@
 # Define your spam function starting on line 5. You
 # can leave the code on line 10 alone for now--we'll
 # explain it soon!
-def spam
+def spam()
+    print("Eggs")
 
 # Define the spam function above this line.
 spam()
