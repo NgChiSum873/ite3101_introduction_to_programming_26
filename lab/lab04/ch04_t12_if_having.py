98e7606728bbeb6c05036443d9dc09242862d6e2
NgChiSum873 <260061806@stu.vtc.edu.hk>
2026-09-24 07:35:04 +0000

    [skip actions] lab/lab04/ch04_t12_if_having.py

--- lab/lab04/ch04_t12_if_having.py
+++ lab/lab04/ch04_t12_if_having.py
@@ -1,10 +1,10 @@
 def using_control_once() -> str:
-    if None:
+    if 2 > 1:
         return "Success #1"
 
 
 def using_control_again() -> str:
-    if 2 ==:
+    if 2 == 1:
         return "Success #2"
 
 
