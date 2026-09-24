28021f75a8f4dfa5038011cc28bd15e24e4a8cbb
NgChiSum873 <260061806@stu.vtc.edu.hk>
2026-09-24 07:36:19 +0000

    lab/lab04/ch04_t13_else.py

--- lab/lab04/ch04_t13_else.py
+++ lab/lab04/ch04_t13_else.py
@@ -1,14 +1,14 @@
 answer = "'Tis but a scratch!"
 
 
-def black_knight() -> bool:
+def black_knight() -> str:
     if answer == "'Tis but a scratch!":
         return True
     else:
         return  # Make sure this returns False
 
 
-def french_soldier() -> bool:
+def french_soldier() -> str:
     if answer == "Go away, or I shall taunt you a second time!":
         return True
     else:
