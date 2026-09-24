ec06bc7e099a7950e3d5c5b6a3ee829dcd922160
NgChiSum873 <260061806@stu.vtc.edu.hk>
2026-09-24 09:01:03 +0000

    lab/lab06/ch06_t06_prac_makes_perfect.py

--- lab/lab06/ch06_t06_prac_makes_perfect.py
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -1,11 +1,13 @@
 from typing import Any
 
+
 def cube(number: int) -> int:
     return number * number * number
 
-def by_three(number:int)-> Any:
-    if number % 3 ==0:
+
+def by_three(number: int) -> Any:
+    if number % 3 == 0:
         return cube(number)
 
-    else;
+    else:
         return False
